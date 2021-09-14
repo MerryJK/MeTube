53b46771e9b5b8b35d4e15794736a497fac73191
MerryJK <60599791+MerryJK@users.noreply.github.com>
2021-09-14 18:29:34 +0900

    First server res

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome =(req, res) => {
+    //return res.send("bb");
+    return res.send("<h1> Welcome to Home </h1>");
+};
+
+app.use(morgan("dev"));
+app.get("/", handleHome);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
